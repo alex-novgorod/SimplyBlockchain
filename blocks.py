f76b3169c57449508b68ea73b97eaaf3decfd0d6
alex-novgorod <alex-novgorod@mail.ru>
2024-06-15 17:59:16 +0300

    new

--- blocks.py
+++ blocks.py
@@ -2,14 +2,21 @@ import hashlib
 import json
 import os
 
+def get_hash(filename):
+    blockchain_dir = os.curdir + '/blocks/'
+    file = open(blockchain_dir + filename, 'rb').read()
+    return hashlib.md5(file).hexdigest()
+
 def writeblock(who, amount, to_whom, prev_hash=''):
-    blockchain_dir = os.curdir + '/SimplyBlockchain/SimplyBlockchain/blocks/'
-    file = str(sorted([int(i) for i in os.listdir(blockchain_dir)])[-1]+1)
+    blockchain_dir = os.curdir + '/blocks/'
+    last_file = sorted([int(i) for i in os.listdir(blockchain_dir)])[-1]
+    prev_hash = get_hash(str(last_file))
+    file_name = str(last_file + 1)
     data = {'who': who,
             'amount': amount,
             'to_whom': to_whom,
             'hash': prev_hash}
-    with open(blockchain_dir + file, 'w') as file:
+    with open(blockchain_dir + file_name, 'w') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 def main():
